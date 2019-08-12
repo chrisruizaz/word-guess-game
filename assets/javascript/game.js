bb2bcfb978107a67b1fef345e873b0845da7bdbd
Christopher Ruiz <chrisruizaz@gmail.com>
2019-08-11 22:40:19 -0700

    added a lot of code to the pages. saved images

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+console.log("my game.js file is attached!");
+//global variables
+var wins = 0;
+var loose = 0;
+var userGuess = "";
+
+// object
+var carObject = {
+  corvette: "corvette",
+  corolla: "corolla",
+  tundra: "tundra",
+  tacoma: "tacoma",
+  tahoe: "tahoe",
+  crv: "crv",
+  van: "dontdoit"
+};
+
+// functions
+//the next line of code: adds an _ for each letter in the word being guessed.
+for (x = 0; x < carObject.corvette.length; x++) {
+  console.log("added an underscore");
+  document.getElementById("word-goes-here").innerHTML +=
+    "<span>&nbsp;_&nbsp</span>";
+}
+
+var lettersWrong = "";
+console.log(lettersWrong);
+
+document.onkeyup = function(event) {
+  userGuess = event.key;
+  for (i = 0; i < carObject.corvette.length; i++)
+    if (userGuess == carObject.corvette[i]) {
+      console.log(`${userGuess} is in this word!`);
+    } else {
+      console.log(`${userGuess} youre stupid!`);
+    }
+};
