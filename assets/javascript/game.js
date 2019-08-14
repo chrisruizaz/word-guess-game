bf8c6bf6d2b2f3e01ae4f439d75ff3c327d88385
Christopher Ruiz <chrisruizaz@gmail.com>
2019-08-13 18:30:50 -0700

    debugged wrong letters being guessed and added multiple times

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,25 +19,30 @@ var carObject = {
 //the next line of code: adds an _ for each letter in the word being guessed.
 for (x = 0; x < carObject.corvette.length; x++) {
   console.log("added an underscore");
-  document.getElementById("word-goes-here").innerHTML +=
-    "<span>&nbsp;_&nbsp</span>";
+  document.getElementById(
+    "word-goes-here"
+  ).innerHTML += `<span id="letter${x}">&nbsp;_&nbsp</span>`;
 }
 
 var lettersWrong;
-console.log(lettersWrong);
 
 document.onkeyup = function(event) {
   userGuess = event.key;
-  for (i = 0; i < carObject.corvette.length; i++) {
-    console.log(userGuess);
-    if (userGuess === carObject.corvette[i]) {
-      wins++;
-      console.log(`${userGuess} is in this word!`);
-    } else {
-      console.log(`${userGuess} youre stupid!`);
-      lose++;
-      document.getElementById("lose-text").innerHTML =
-        "<span>losses=" + lose + "</span>";
+
+  //wrong letter
+  if (carObject.corvette.indexOf(userGuess) == -1) {
+    console.log(`${userGuess} is not in this word.`);
+    document.getElementById("lose-text").innerHTML += userGuess;
+  } else if (carObject.corvette.indexOf(userGuess) >= 0) {
+    //the location of the correct letter.
+    for (i = 0; i < carObject.corvette.length; i++) {
+      if (userGuess === carObject.corvette[i]) {
+        //correct letter in location
+        console.log(`correct letter at location ${i}`);
+        document.getElementById(`letter${i}`).innerHTML = userGuess;
+      } else {
+        //these are not the location
+      }
     }
   }
 };
